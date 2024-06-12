73ebe299c09124879a3f12a42404f5686eda0bbe
vDevPetro <victor.lima1.prestserv@petrobras.com.br>
2024-06-12 17:10:04 -0300

    Funções de leitura-AS

--- index.js
+++ index.js
@@ -1,24 +1,59 @@
 import express from 'express';
-import db from './firebase.js'; // Certifique-se de que o caminho está correto
-
-const app = express();
+import db from './firebase.js'; 
 
 // REST API
+const app = express();
 
-// GET
-app.get('/teste', async (req, res) => {
+// AS CRUD
+
+// GET ALL https://apisiproj.vercel.app/as
+app.get('/as', async (req, res) => {
     try {
-        const snapshot = await db.collection('teste').get();
+        const snapshot = await db.collection('AS').get();
         const itens = snapshot.docs.map(doc => ({
-            ...doc.data(),
-            id: doc.id
+            id: doc.id,
+            ...doc.data()
         }));
         res.json(itens);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }    
+});
+
+// GET https://apisiproj.vercel.app/as/:id
+app.get('/as/:id', async (req, res) => {
+    try {
+        const docRef = db.collection('AS').doc(req.params.id);
+        const docSnap = await docRef.get();
+
+        if (!docSnap.exists) {
+            res.status(400).json({message: 'Documento não encontrado'})
+        }
+
+        res.json(docSnap.data());
     } catch (error) {
         res.status(500).send(error.message);
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+// POST https://apisiproj.vercel.app/as
+app.post('/as', (req, res) => {
+
 });
+
+// PUT https://apisiproj.vercel.app/as/:id
+app.put('/as/:id', (req, res) => {
+    const id = req.params.id;
+    
+});
+
+// DELETE https://apisiproj.vercel.app/as/:id
+app.delete('/as/:id', (req, res) => {
+    const id = req.params.id;
+    
+});
+
+app.listen(3000);
+
+
+
