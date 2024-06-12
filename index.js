38bf541aa2960392f3b0352ab80d2a28e53e9740
vDevPetro <victor.lima1.prestserv@petrobras.com.br>
2024-06-12 14:11:27 -0300

    .env.local configuration

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 import express from 'express';
-import admin from 'firebase-admin';
+import db from './firebase.js'; // Certifique-se de que o caminho está correto
 
 const app = express();
-
-admin.initializeApp({
-    credential: admin.credential.cert("ServiceAccountKey.json")
-});
 
 // REST API
 
 // GET
-app.get('/teste', (req, res) => {
-    admin.firestore()
-        .collection('teste')
-        .get()
-        .then(snapshot => {
-            const itens = snapshot.docs.map(doc => ({
-                ...doc.data(),
-                id: doc.id
-            }))
-            res.json(itens)
-        })
-})
+app.get('/teste', async (req, res) => {
+    try {
+        const snapshot = await db.collection('teste').get();
+        const itens = snapshot.docs.map(doc => ({
+            ...doc.data(),
+            id: doc.id
+        }));
+        res.json(itens);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
 
-app.listen(3000)
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
