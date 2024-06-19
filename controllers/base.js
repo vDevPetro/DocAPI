4a5b83d9959be3f42218341e1eef90eef8f9a2c3
X-MCB <matheustoselli@outlook.com>
2024-06-19 14:47:13 -0300

    reorganizacao

---
+++ controllers/base.js
@@ -0,0 +1,68 @@
+import db from './firebase.js'; 
+
+const getAllBase = async (req, res) => {
+  try {
+      const snapshot = await db.collection('AS').get();
+      const itens = snapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+      }));
+      res.json(itens);
+  } catch (error) {
+      res.status(500).send(error.message);
+  }    
+}
+
+const getBaseId = async (req, res) => {
+  try {
+      const docRef = db.collection('AS').doc(req.params.id);
+      const docSnap = await docRef.get();
+
+      if (!docSnap.exists) {
+          res.status(400).json({message: 'Documento não encontrado'})
+      }
+
+      res.json(docSnap.data());
+  } catch (error) {
+      res.status(500).send(error.message);
+  }
+}
+
+const postBase = async (req, res) => {
+  try {
+    const newItem = req.body; 
+    const docRef = await db.collection("AS").add(newItem);
+    res.status(201).json({ id: docRef.id });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+const putBase =  async (req, res) => {
+  try {
+    const itemId = req.params.id;
+    const updatedData = req.body; 
+    await db.collection("AS").doc(itemId).set(updatedData, { merge: true });
+    res.status(200).send("Item atualizado com sucesso");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+const deleteBase = async (req, res) => {
+  try {
+    const itemId = req.params.id;
+    await db.collection("AS").doc(itemId).delete();
+    res.status(200).send("Item excluído com sucesso");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+module.exports = {
+  getAllBase,
+  getBaseId,
+  postBase,
+  putBase,
+  deleteBase,
+}
