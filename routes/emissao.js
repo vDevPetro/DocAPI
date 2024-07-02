310652740f21ca3a0577d73a5e9f4d9c2c9b42b0
Matheus Correia <matheustoselli@outlook.com>
2024-07-02 09:17:22 -0300

    GetAllEmissao, GetByIdEmissao (#12)

--- routes/emissao.js
+++ routes/emissao.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import{
   postEmissao,
-  getAllEmissao
+  getAllEmissao,
+  getByIdEmissao
 }from '../controllers/emissao.js';
 
 const router = express.Router();
 
 router.get('/', getAllEmissao);
+router.get('/:id', getByIdEmissao); 
 router.post('/', postEmissao);
 
 export default router;
